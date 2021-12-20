420f524aaf860a086fc22468c64f112894a638a4
ruppin <ruppin742022@gmail.com>
2021-12-20 16:27:43 +0200

    ex 3
    ex 3 finish files

---
+++ Scripts/ajaxCallsWithKey.js
@@ -0,0 +1,15 @@
+﻿function ajaxCall2(method, api, data, successCB, errorCB) {
+    $.ajax({
+        type: method,
+        url: api,
+        data: data,
+        cache: false,
+        headers: {
+            'x-rapidapi-key': '14a9b7f875mshc8fa6fa346cbe60p1e8815jsnc82c85354b32',   // replace it with your own key
+        },
+        contentType: "application/json",
+        dataType: "json",
+        success: successCB,
+        error: errorCB
+    });
+}
